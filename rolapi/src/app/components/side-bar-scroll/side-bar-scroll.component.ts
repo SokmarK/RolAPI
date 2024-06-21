ad0df79e37f492845b29067787cfc7845aed7ddd
SokmarK <hleontorres05@gmail.com>
2024-06-20 20:38:52 -0500

    componentes

---
+++ rolapi/src/app/components/side-bar-scroll/side-bar-scroll.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-bar-scroll',
+  standalone: true,
+  imports: [],
+  templateUrl: './side-bar-scroll.component.html',
+  styleUrl: './side-bar-scroll.component.css'
+})
+export class SideBarScrollComponent {
+
+}
