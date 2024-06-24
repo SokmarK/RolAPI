2ffc8e4f7e2e720114ca7822a06dfc339bcb406f
AndresMosquera9005 <andrubjj@gmail.com>
2024-06-24 10:55:21 -0500

    se creo la carpeta de rutas, se modifico

---
+++ rolapi/src/app/components/rutas/rutas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rutas',
+  standalone: true,
+  imports: [],
+  templateUrl: './rutas.component.html',
+  styleUrl: './rutas.component.css'
+})
+export class RutasComponent {
+
+}
