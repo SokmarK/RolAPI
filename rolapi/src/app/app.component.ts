d6af9d71fc6b9d78e95d5f5cf6993a86ce29c91a
Felipe00726 <felipe21palacio@gmail.com>
2024-06-24 20:01:05 -0500

    comit 2

--- rolapi/src/app/app.component.ts
+++ rolapi/src/app/app.component.ts
@@ -2,14 +2,19 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BodyComponent } from './components/body/body.component';
+import { SearchComponent } from './components/search/search.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, BodyComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+   selector: 'app-root',
+   standalone: true,
+   imports: [
+      RouterOutlet,
+      BodyComponent,
+      SearchComponent
+   ],
+   templateUrl: './app.component.html',
+   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'rolapi';
+   title = 'rolapi';
 }
