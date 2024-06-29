b8327a83fd896138040891272c1b42bf9c9c00f8
Felipe00726 <felipe21palacio@gmail.com>
2024-06-28 19:12:41 -0500

    Creacion vista generos admin

--- rolapi/src/app/components/offcanvas/offcanvas.component.ts
+++ rolapi/src/app/components/offcanvas/offcanvas.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { ServicesService } from '../../services/services.service';
 
 @Component({
-  selector: 'app-offcanvas',
-  standalone: true,
-  imports: [],
-  templateUrl: './offcanvas.component.html',
-  styleUrl: './offcanvas.component.css'
+    selector: 'app-offcanvas',
+    standalone: true,
+    imports: [],
+    templateUrl: './offcanvas.component.html',
+    styleUrl: './offcanvas.component.css'
 })
 export class OffcanvasComponent {
-
+    admin: any
+    constructor(private _servicio: ServicesService) {
+    }
+    ngOnInit() {
+        this.admin = this._servicio.verficarRol()
+    }
 }
