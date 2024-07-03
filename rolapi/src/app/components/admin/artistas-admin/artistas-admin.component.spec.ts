145d6f01d104b49322a156c4d173ef55eeae2e08
SokmarK <hleontorres05@gmail.com>
2024-07-03 17:59:15 -0500

    cambios para nuevo merge

--- rolapi/src/app/components/admin/artistas-admin/artistas-admin.component.spec.ts
+++ rolapi/src/app/components/admin/artistas-admin/artistas-admin.component.spec.ts
@@ -3,21 +3,21 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ArtistasAdminComponent } from './artistas-admin.component';
 
 describe('ArtistasAdminComponent', () => {
-  let component: ArtistasAdminComponent;
-  let fixture: ComponentFixture<ArtistasAdminComponent>;
+    let component: ArtistasAdminComponent;
+    let fixture: ComponentFixture<ArtistasAdminComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ArtistasAdminComponent]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [ArtistasAdminComponent]
+        })
+            .compileComponents();
 
-    fixture = TestBed.createComponent(ArtistasAdminComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(ArtistasAdminComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
